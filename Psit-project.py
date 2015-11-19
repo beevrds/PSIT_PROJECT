26b725dc45e365c68fa0b70e78dba2d0b1fc6438
kijtana-ch <s7070006@kmitl.ac.th>
2015-11-19 16:44:01 +0700

    Delete GUI

--- Psit-project.py
+++ Psit-project.py
@@ -1,27 +1,7 @@
 """the data processing project PSIT 2015"""
-from tkinter import *
 from matplotlib import pyplot
 from numpy import arange
-
-window = Tk()
-window.geometry("400x400+900+0")
 main_data = {"Resident":50 , "small company":300 , "medium company": 1000, "large company": 2345,"Government":200}
-def main():
-    """main of this call of all element of program"""
-    menu()#call the menu of program
-    main_window()
-
-def main_window():
-    label = Label(window, text = "Volt of voluminous", bg = "white")
-    label.pack()
-
-def menu():
-    """create menutab on window"""
-    menubar = Menu(window)
-    menu_about = Menu(menubar)#Create menu tab
-    menu_about.add_command(label = "Credit")#add menu to menu tab
-    menubar.add_cascade(label = "Creator", menu = menu_about)#add sub menu to menu
-    window.config(menu = menubar)
 
 def bar_graph(data, value):
     """create the bar graph form data"""
@@ -30,4 +10,4 @@ def bar_graph(data, value):
     pyplot.bar(pos,value)
     pyplot.show()
 
-main()
+bar_graph(main_data.keys(), main_data.values())
