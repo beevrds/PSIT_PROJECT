63fb0d70f29dfdb6499edf292607fb1ec9a79292
kijtana-ch <s7070006@kmitl.ac.th>
2015-12-03 20:47:21 +0700

    Pie chart
    Create pie chat and Improve bar graph function

--- Psit-project.py
+++ Psit-project.py
@@ -5,9 +5,19 @@ main_data = {"Resident":50 , "small company":300 , "medium company": 1000, "larg
 
 def bar_graph(data, value):
     """create the bar graph form data"""
+    bar_width = 0.45
     pos = arange(len(data))
-    pyplot.xticks(pos+0.4,data)
-    pyplot.bar(pos,value)
+    pyplot.xticks(pos+0.25,data)
+    pyplot.bar(pos,value, bar_width, color="lightgreen")
     pyplot.show()
 
-bar_graph(main_data.keys(), main_data.values())
+def pie_chart(data, value):
+    value = tuple(value)
+    colors = ["grey", "gold", "lightskyblue", "lightcoral", "lightgreen"]
+    pyplot.pie(value, labels=data, colors=colors,
+            autopct="%1.1f%%", shadow=True, startangle=90)
+    pyplot.axis("equal")
+    pyplot.show()
+    
+#bar_graph(main_data.keys(), main_data.values())
+pie_chart(main_data.keys(), main_data.values())
